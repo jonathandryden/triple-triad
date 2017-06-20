51eff51c71f5f98ecea4a6a447e79a1f383f0379
Jonathan Dryden <jwdryden@gmail.com>
2017-06-20 19:27:03 -0400

    Testing

--- server/app.js
+++ server/app.js
@@ -1,15 +1,16 @@
-const app = require("express")(),
+const express = require("express");
+const app = express(),
 config = require("./config.js");
 http = require("http").Server(app),
 io = require("socket.io")(http),
-server = require("./server.js")(io);
+server = require("./server.js")(io),
+path = require("path");
 
 const Logger = require("./Logger.js"),
 User = require("./User.js");
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/public/index.html");
-});
+app.use(express.static(path.join(__dirname, "public")));
+
 http.listen(config.server.port, function(){
   console.log("listening on *:" + config.server.port);
 });
