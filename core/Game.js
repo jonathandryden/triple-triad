060569d72f28538332ece09cdb2964f97e3a6f74
Jonathan Dryden <jwdryden@gmail.com>
2017-06-20 21:26:53 -0400

    refactor game

--- core/Game.js
+++ core/Game.js
@@ -1,10 +1,10 @@
 const Player = require("./Player.js"),
-Card = require("./Card.js"),
-CardDb = require("./cards.json"),
-Errors = require("./Errors");
+  Card = require("./Card.js"),
+  CardDb = require("./cards.json"),
+  Errors = require("./Errors");
 
 // utilities
-var hasEmptyCells = function(board) {
+const hasEmptyCells = function(board) {
   for (let i = 0, len = board.length; i < len; i++) {
     for (let j = 0, len2 = board[i].length; j < len2; j++) {
       if (!board[i][j]) return true;
@@ -13,94 +13,72 @@ var hasEmptyCells = function(board) {
   return false;
 }
 
-var numberOfCards = function(cards) {
-  let result = 0;
-  for (let i = 0; i < cards.length; i++) {
-    if (cards[i]) {
-      result++;
-    }
-  }
-  return result;
-}
-
-var compareCards = function(card, dir, opposingCard, def) {
+const compareCards = function(card, dir, opposingCard, def) {
   if (opposingCard) {
-    if (opposingCard.color != card.color && card.rank[dir] > opposingCard.rank[def]) {
+    if (opposingCard.color !== card.color && card.rank[dir] > opposingCard.rank[def]) {
       opposingCard.color = card.color;
     }
   }
 }
 
 class Game {
-  constructor(game) {
-    if (game) {
-      this.name = game.name;
-      this.isGameOver = game.isGameOver;
-      this.board = game.board;
-      this.players = game.players;
-      this.score = game.score;
+  constructor(importedGame) {
+    if (importedGame) {
+      this.name = importedGame.name;
+      this.isGameOver = importedGame.isGameOver;
+      this.board = importedGame.board;
+      this.players = importedGame.players;
+      this.score = importedGame.score;
     } else {
       this.name = "Triple Triad";
       this.isGameOver = false;
+      this.playerTurn = 1;
       this.board = [
         [undefined, undefined, undefined],
         [undefined, undefined, undefined],
         [undefined, undefined, undefined]
       ];
-      this.players = [new Player(1, this.generateCards(1))
-        , new Player(2, this.generateCards(2))];
+      this.players = [new Player(), new Player()];
       this.score = [0, 0];
+      dealCards(5);
     }
   }
 
-  generateCards(player) {
-    var generatedCards = [];
-    for (let i = 0; i<5; i++) {
-      var randomId = Math.floor((Math.random() * 10));
-      for (let j = 0, len = CardDb.length; j<len; j++){
-        if (CardDb[j].id === randomId) {
-          var card = new Card(CardDb[j]);
-          if (player === 2) {
-            card.color = true;
-          }
-          generatedCards.push(card);
+  dealCards(numberOfCards) {
+    let tempDeck = [],
+      randomCardId,
+      card;
+    for (let k = 0; k < 2; k++) {
+      for (let i = 0; i < numberOfCards; i++) {
+        randomCardId = Math.floor((Math.random() * CardDb.length));
+        for (let j = 0; j < CardDb.length; j++) {
+          card = CardDb[j];
+          if (k === 0) card.color = "red";
+          tempDeck.push(card);
         }
       }
+      this.players.hand = [...tempdDeck];
+      tempDeck = [];
     }
-    return generatedCards;
   }
 
-  placeCard(player, cardIndex, location, callBack) {
-    let x = Number(location.x),
-    y = Number(location.y),
-    card = null;
+  playMove(playerNumber, cardId, xLocation, yLocation) {
+    let x = xLocation,
+      y = yLocation,
+      cardIndex = this.player[playerNumber - 1].hand.findIndex(item => item.id === cardId),
+      card = this.players[playerNumber - 1].hand[cardIndex],
+      opposingCard;
 
-    if ((numberOfCards(this.players[0].cards) === numberOfCards(this.players[1].cards) && Number(player) !== 0) 
-    || (numberOfCards(this.players[0].cards) < numberOfCards(this.players[1].cards) && Number(player) !== 1)) {
-      callBack(new Errors.InvalidAction("It is not your turn to move."));
-      return;
-    }
-
-    if (this.board[y][x] != undefined) {
-      callBack(new Errors.InvalidAction("This area of the field is occupied already."));
-      return;
-    }
-
-    card = this.players[player].cards[cardIndex];
-    
-    if (!card) {
-      callBack(new Errors.InvalidAction("The card played does not exist in the players hand."));
-      return;
+    if (this.playerTurn !== playerNumber || this.board[y][x] || !card) {
+      // err
     }
 
     this.board[y][x] = card;
-    this.players[player].cards[cardIndex] = undefined;
-    
-    var opposingCard = undefined;
+    this.player[playerNumber].hand.splice(cardIndex, 1);
 
     // check top
     if (y - 1 >= 0) {
-      compareCards(card, "top", this.board[y-1][x], "bottom");
+      compareCards(card, "top", this.board[y - 1][x], "bottom");
     }
 
     // check right
@@ -110,29 +88,27 @@ class Game {
 
     // check bottom
     if (y + 1 <= 2) {
-      compareCards(card, "bottom", this.board[y+1][x], "top");
+      compareCards(card, "bottom", this.board[y + 1][x], "top");
     }
 
     // check left
     if (x - 1 >= 0) {
-      compareCards(card, "left", this.board[y][x-1], "right");
+      compareCards(card, "left", this.board[y][x - 1], "right");
     }
 
     this.updateScore();
-
-    callBack(null, this.score);
   }
 
   updateScore() {
     this.score = [0, 0];
 
-    for (let i = 0, len = this.board.length; i < len; i++) {
-      for (let j = 0, len2 = this.board[i].length; j < len; j++) {
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[i].length; j++) {
         if (this.board[i][j]) {
-          if (this.board[i][j].color) {
-            this.score[1]++;
-          } else {
+          if (this.board[i][j].color === "red") {
             this.score[0]++;
+          } else {
+            this.score[1]++;
           }
         }
       }
